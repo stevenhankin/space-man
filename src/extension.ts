5b1c81edf092eef2eb9e3584365ca33bd963d508
Steven Hankin <steven.hankin@gmail.com>
2021-03-26 15:10:15 +0000

    Example using google charts

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { PathLike, readdir } from "fs";
-// import { directoryTree } from "directory-tree";
-import { DirNode, processDir } from "dir-roll-up";
-
 import * as vscode from "vscode";
-import dirTree = require("directory-tree");
-import { sep } from "path";
 import { createHash } from "crypto";
 
+import dirRollUp from "dir-roll-up";
+import { DirNode } from "dir-roll-up";
+
 /**
- * Used for hashing a directory name
- * @param txt
- * @returns hash in hex format
+ * Name, Parent, Size
+ *
+ * TODO: add Colour scale
  */
-const encode = (txt: string): string =>
-  createHash("md5").update(txt).digest("hex");
+type TreeMapTuple = [string, string | null, string | number];
+type TreeMapView = TreeMapTuple[];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -29,82 +26,112 @@ export function activate(context: vscode.ExtensionContext) {
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "space-man.spaceUsage",
-    () => {
+    async () => {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from Space Man!");
+      vscode.window.showInformationMessage("Space Man started...");
 
-      // const path: PathLike = ".";
-      // readdir(path, {}, (err, files) => {
-      //   console.log({ files });
-      // });
+      const startTime = new Date().getTime();
 
-      // const moo = async () => {
-      //   for await (let x of processDir(".")) {
-      //     console.log(x);
-      //   }
-      // };
-      // moo();
-
-      type TreeNode = {
-        fileCount: number;
-        dirName: string;
-        parentDirHash: string | undefined;
-        sizeOfDir: number;
-        // totalSize: number;
-      };
-
-      type TreeModel = {
-        [key: string]: TreeNode;
-      };
-
-      const treeModel: TreeModel = {};
-
-      const tree = dirTree(
-        "/Users/stevenhankin/projects/space-man",
-        {},
-        (item, path, stats) => {
-          if (item.type === "directory") {
-            console.log(item);
-          }
-          // Get the directory the file resides in
-          const dirName = path.substr(0, path.lastIndexOf(sep));
-          // and convert to a hash
-          const dirHash = encode(dirName);
-          // and the parent dir (so we can keep a pointer to it for roll-up of space)
-          const parentDirName = dirName.substr(0, dirName.lastIndexOf(sep));
-          // and also its hash
-          const parentDirHash = encode(parentDirName);
-          // if it's the top dir level it may not exist
-          if (treeModel[parentDirHash] === undefined) {
-            treeModel[parentDirHash] = {
-              fileCount: 0,
-              sizeOfDir: 0,
-              dirName: parentDirName,
-              parentDirHash: encode(
-                parentDirName.substr(0, parentDirName.lastIndexOf(sep))
-              ),
-            };
-          }
-          // and see if it is already in out tree model
-          let treeDataObj = treeModel[dirHash];
-          if (treeDataObj === undefined) {
-            // if not, we intialise to a default
-            treeModel[dirHash] = {
-              fileCount: 1,
-              sizeOfDir: item.size,
-              dirName,
-              parentDirHash,
-            };
-          } else {
-            // otherwise we can update the attributes
-            treeDataObj.fileCount++;
-            treeDataObj.sizeOfDir += item.size;
-          }
-        }
+      // Create and show a new webview
+      const panel = vscode.window.createWebviewPanel(
+        "spaceman", // Identifies the type of the webview. Used internally
+        "Space Man", // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        { enableScripts: true } // Webview options. More on these later.
       );
-      console.log(treeModel);
+
+      let nodeCount = 0;
+      panel.webview.html = getWebviewContent([
+        ["Name", "Parent", "Size (bytes)"],
+      ]);
+
+      let generator = dirRollUp("/Users/stevenhankin/projects/space-man", {
+        includePartial: true,
+      });
+
+      /**
+       * One-to-one map of Node ID to Node
+       */
+      let idToDirMap = new Map<string, DirNode>();
+
+      /**
+       * Dir names are not unique, so store an array of nodes
+       * against each name so that a suffix suffix can be added
+       * to satisfy the google chart's need for a unique name
+       */
+      let nameInfoMap = new Map<string, DirNode[]>();
+
+      const interval1 = setInterval(async () => {
+        const dirNode = await generator.next();
+        const v = dirNode.value;
+        idToDirMap.set(v.id, v);
+        /**
+         * If Node isn't already in the list for it's name..
+         */
+        const nodesForName = nameInfoMap.get(v.dirName) || [];
+        if (!nodesForName.some((n) => n.id === v.id)) {
+          /**
+           * ..add it in
+           */
+          nodesForName.push(v);
+          nameInfoMap.set(v.dirName, nodesForName);
+          nodeCount++;
+          console.log(`${v.dirName} has size ${v.sizeOfDir}`);
+        }
+        if (dirNode.done) {
+          clearInterval(interval1);
+          const endTime = new Date().getTime();
+          vscode.window.showInformationMessage(
+            `Space Man completed in ${Math.floor(
+              (endTime - startTime) / 1000
+            )} seconds 🚀`
+          );
+        }
+      }, 1);
+
+      /**
+       * Unique directory name with a numerical suffix if it occurs more than once
+       * Will be null for the parent of the root (since there is no higher node)
+       * @param id of the node
+       * @returns display name
+       */
+      const getNodeName = (id: string | null) => {
+        if (id === null) {
+          return null;
+        }
+        const node = idToDirMap.get(id);
+        if (node === undefined) {
+          throw Error(`No node matches id ${id}`);
+        }
+        const nameArray = nameInfoMap.get(node.dirName);
+        if (nameArray === undefined) {
+          throw Error(`No name array for directory ${node.dirName}`);
+        }
+        const idx = nameArray.findIndex((n) => n.id === id);
+        /**
+         * If the node is not first in the array
+         * it means there is a duplicate name
+         * and this needs a suffix to make the name appear unique
+         */
+        if (idx > 0) {
+          return `${node.dirName} (${idx})`;
+        }
+        return `${node.dirName}`;
+      };
+
+      const interval2 = setInterval(async () => {
+        const treeMapView: TreeMapView = [["Name", "Parent", "Size (bytes)"]];
+        idToDirMap.forEach((node) => {
+          treeMapView.push([
+            getNodeName(node.id) as string,
+            getNodeName(node.parent),
+            node.sizeOfDir,
+          ]);
+        });
+        panel.webview.html = getWebviewContent(treeMapView);
+      }, 100);
     }
   );
 
@@ -113,3 +140,42 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent(treeMapView: TreeMapView) {
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['treemap']});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable(${JSON.stringify(
+          treeMapView
+        )});
+
+        tree = new google.visualization.TreeMap(document.getElementById('chart_div'));
+
+        tree.draw(data, {
+          minColor: '#f00',
+          midColor: '#ddd',
+          maxColor: '#0d0',
+          headerHeight: 15,
+          fontColor: 'black',
+          showScale: true,
+          maxDepth: 3,
+          generateTooltip: showStaticTooltip,
+        });
+
+        function showStaticTooltip(row, size, value) {
+          return '<div style="background:#fd9; padding:10px; border-style:solid">' +
+                 size +'</div>';
+        }
+      }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>`;
+}
